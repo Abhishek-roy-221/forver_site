58bf44b51bbda7811b9f39263c616c2b6381b689
Abhishek Roy <abhishek8400roy@gmail.com>
2025-08-23 20:08:30 +0530

    orders page done

--- src/context/ShopConetxt.jsx
+++ src/context/ShopConetxt.jsx
@@ -1,6 +1,7 @@
 import { createContext, use, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -10,8 +11,8 @@ const ShopContextProvider = (props)=>{
     const delivery_fee=10;
 const [search,setSearch] = useState('');
 const [showSearch,setShowSearch] = useState(false);
-
 const [cartItems,setCartItems] = useState({});
+const navigate = useNavigate();
 
 const addToCart = async (itemId,size)=>{
 
@@ -81,7 +82,8 @@ for(const items in cartItems){
 
     const value = {
 products , currency,delivery_fee,search,setSearch,showSearch,setShowSearch,cartItems,addToCart,
-getCartCount,updateQuantity,getCartAmount
+getCartCount,updateQuantity,getCartAmount,
+navigate
     }
     return <ShopContext.Provider value={value}>
         {props.children}
